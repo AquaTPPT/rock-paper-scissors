1fc9ad89f5298f0619d270e9f2e2b20f7802113e
AquaTPPT <paulostiago717@gmail.com>
2026-01-15 17:15:47 +0000

    Completed the UI.

--- assets/rock-paper-scissors.js
+++ assets/rock-paper-scissors.js
@@ -1,6 +1,19 @@
-
 let humanScore = 0;
 let computerScore = 0;
+
+const container = document.querySelector('#container');
+const results = document.querySelector('#results');
+
+const text = document.createElement('p');
+results.appendChild(text)
+
+const counter = document.createElement('p');
+results.appendChild(counter);
+
+const startGame = document.createElement('button');
+startGame.textContent = 'Start Game';
+container.appendChild(startGame);
+startGame.addEventListener('click', () => {getHumanChoice()});
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -18,46 +31,83 @@ function getComputerChoice() {
     }
 }
 
+
 function getHumanChoice() {
-    let choice = prompt('Rock, Paper or Scissors?');
-    let normalizedChoice = choice.toLowerCase();
-    return normalizedChoice;
+
+    const rockButton = document.createElement('button');
+    rockButton.textContent = 'Rock';
+    container.appendChild(rockButton);
+
+    const paperButton = document.createElement('button');
+    paperButton.textContent = 'Paper';
+    container.appendChild(paperButton);
+
+    const scissorsButton = document.createElement('button');
+    scissorsButton.textContent = 'Scissors';
+    container.appendChild(scissorsButton);
+
+    rockButton.addEventListener('click', function() {
+        choice = 'rock';
+        playRound(choice, getComputerChoice())
+        container.removeChild(rockButton);
+        container.removeChild(paperButton);
+        container.removeChild(scissorsButton);
+    });
+    paperButton.addEventListener('click', function() {
+        choice = 'paper';
+        playRound(choice, getComputerChoice())
+        container.removeChild(rockButton);
+        container.removeChild(paperButton);
+        container.removeChild(scissorsButton);
+    });
+    scissorsButton.addEventListener('click', function() {
+        choice = 'scissors';
+        playRound(choice, getComputerChoice())
+        container.removeChild(rockButton);
+        container.removeChild(paperButton);
+        container.removeChild(scissorsButton);
+    });
+
 }
 
 function playRound(humanChoice, computerChoice) {
+    
+    if (humanScore == 5 || computerScore == 5) {
+        humanScore = 0;
+        computerScore = 0;
+    }
+
     let result;
     if (humanChoice == 'rock' && computerChoice == 'rock' || 
         humanChoice == 'paper' && computerChoice == 'paper' ||
         humanChoice == 'scissors' && computerChoice == 'scissors') {
-            result = 'Tie!'  
+            result = 'Tie!';
+            endResult();
         }
         else if (humanChoice == 'rock' && computerChoice == 'paper' || 
         humanChoice == 'paper' && computerChoice == 'scissors' ||
         humanChoice == 'scissors' && computerChoice == 'rock') {
             ++computerScore;
             result = 'You lose!';
+            endResult();
         }
         else {
             ++humanScore;
             result = 'You win!';
+           
         }
 
-    console.log(result)
-    console.log(computerScore + '  ' + humanScore);
-    return result;
+    text.textContent = `${result}`;
+    counter.textContent = `${humanScore} - ${computerScore}`; 
+    endResult();
 }
 
-function playGame() {
-
-    let endResult;
-
-    if (computerScore < humanScore) {
-        endResult = 'You won the game! Congratulations!'; }
-    else if (computerScore = humanScore) {
-        endResult = "It's a draw! Good Game!"; }
-    else {
-        endResult ="The computer wins! Good Game!" }
+function endResult() {
+    if (humanScore == 5) {
+        text.textContent = '\n You won the game! Congratulations!'
+    }
+    else if (computerScore == 5){
+        text.textContent = '\n The computer won! Maybe next time.'
+    }
     
-    return console.log(endResult)
 }
-playGame();
